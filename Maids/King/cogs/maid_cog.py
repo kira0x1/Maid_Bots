5379038f1bdf66a7e8dcd17875f4e5376ea6c7ed
King Arthur <king.art.arthur@gmail.com>
2016-09-24 13:41:29 -0400

    a

--- Maids/King/cogs/maid_cog.py
+++ Maids/King/cogs/maid_cog.py
@@ -1,61 +1,33 @@
 from discord.ext import commands
 import discord
 import asyncio
-from bs4 import BeautifulSoup
-import requests
-import urllib.request
-import random
-import time
-
-from cleverbot import Cleverbot
 
 class maid_cog:
-    "Fun commands"
-
-    #woop woop
 
     def __init__(self, bot):
         self.bot = bot
-        self.cb = Cleverbot()
-        self.commentList = []
-        print(len(self.commentList))
-        self.previousTime = time.time()
-        self.cd = 0.0
-        self.initialized = False;
-        self.beenSaid = []
         self.king = None
 
 
+    async def on_ready(self):
+        self.bot.loop.create_task(self.replyLoop())
 
     async def replyLoop(self):
         i= 0
         while(True):
-            #if (len(self.commentList) > 0):
-            await self.bot.send_message(self.bot.get_channel(str(226771859964690432)), (str(i)) + " These are my bots")
-            i+=1
-                #self.commentList.pop()
-            await asyncio.sleep(3.0)
-
-
-    async def progressLoop(self):
-        while(True):
-            await self.bot.send_message(self.bot.get_channel(str(226777134721531904)),"t!rank")
-            await self.bot.send_message(self.bot.get_channel(str(226804409659686913)), "t!daily")
-            await asyncio.sleep(600)
+            try:
+                await self.bot.send_message(self.bot.get_channel(str(226771859964690432)), str(i) + " testing the length of my dong, you know what I am saying?")
+                i+=1
+                await asyncio.sleep(5.0)
+            except:
+                print("replyLoop error")
+                self.bot.loop.create_task(self.replyLoop())
+                break
 
 
     async def on_message(self, message):
-        if(not self.initialized):
-            self.initialized = True;
-            self.king = message.author.mention
-            self.bot.loop.create_task(self.replyLoop())
-            self.bot.loop.create_task(self.progressLoop())
         if message.author == self.bot.user:
-            if (message.content == "t!rank"):
-                await self.bot.delete_message(message)
-
-
-
+            await self.bot.delete_message(message)
 
 def setup(bot):
     bot.add_cog(maid_cog(bot))
--- Maids/maidMio/cogs/maid_cog.py
+++ Maids/maidMio/cogs/maid_cog.py
@@ -19,9 +19,11 @@ class maid_cog:
             try:
                 await self.bot.send_message(self.bot.get_channel(str(226771859964690432)), str(i) + " testing the length of my dong, you know what I am saying?")
                 i+=1
-                await asyncio.sleep(3.0)
+                await asyncio.sleep(5.0)
             except:
                 print("replyLoop error")
+                self.bot.loop.create_task(self.replyLoop())
+                break
 
     async def progressLoop(self):
         while(True):
@@ -33,6 +35,8 @@ class maid_cog:
                 await asyncio.sleep(600)
             except:
                 print("progressLoop error:")
+                self.bot.loop.create_task(self.progressLoop())
+                break
 
 
     async def on_message(self, message):
