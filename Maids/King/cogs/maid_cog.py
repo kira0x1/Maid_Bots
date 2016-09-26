cdc0a181897d05c346b609ca4b594f8e664a01bd
King Arthur <king.art.arthur@gmail.com>
2016-09-26 08:13:17 -0400

    initlized = false

--- Maids/King/cogs/maid_cog.py
+++ Maids/King/cogs/maid_cog.py
@@ -7,22 +7,49 @@ class maid_cog:
     def __init__(self, bot):
         self.bot = bot
         self.king = None
+        self.initialized = False;
 
 
     async def on_ready(self):
-        self.bot.loop.create_task(self.replyLoop())
+        if(not initialized):
+            self.bot.loop.create_task(self.replyLoop())
+            self.bot.loop.create_task(self.progressLoop())
+            initialized = True
 
     async def replyLoop(self):
         i= 0
+        print(str(self.bot.user) + " has Entered the reply loop!\n")
         while(True):
             try:
                 await self.bot.send_message(self.bot.get_channel(str(226771859964690432)), str(i) + " testing the length of my dong, you know what I am saying?")
                 i+=1
-                await asyncio.sleep(5.0)
+                await asyncio.sleep(10.2)
             except:
                 print("replyLoop error")
                 self.bot.loop.create_task(self.replyLoop())
                 break
+        print("\n\n\n\n" + str(self.bot.user) + " has broken out of the reply loop!\n\n\n\n")
+
+    async def progressLoop(self):
+        while(True):
+            try:
+                await self.bot.send_message(self.bot.get_channel(str(226777134721531904)),"t!rank")
+                if(self.king):
+                    await self.bot.send_message(self.bot.get_channel(str(226804409659686913)), "t!rep " + self.king)
+                    await self.bot.send_message(self.bot.get_channel(str(226804409659686913)), "t!daily " + self.king)
+                await asyncio.sleep(1800)
+            except:
+                print("progressLoop error:")
+                self.bot.loop.create_task(self.progressLoop())
+                break
+
+
+    async def on_message(self, message):
+        if message.content == 'make me king':
+            self.king = message.author.mention
+            print(self.king + " is now King")
+        if message.author == self.bot.user:
+            await self.bot.delete_message(message)
 
 def setup(bot):
     bot.add_cog(maid_cog(bot))
--- Maids/maid_cog.py
+++ Maids/maid_cog.py
@@ -7,23 +7,28 @@ class maid_cog:
     def __init__(self, bot):
         self.bot = bot
         self.king = None
+        self.initialized = False;
 
 
     async def on_ready(self):
-        self.bot.loop.create_task(self.replyLoop())
-        self.bot.loop.create_task(self.progressLoop())
+        if(not self.initialized):
+            self.bot.loop.create_task(self.replyLoop())
+            self.bot.loop.create_task(self.progressLoop())
+            self.initialized = True
 
     async def replyLoop(self):
         i= 0
+        print(str(self.bot.user) + " has Entered the reply loop!\n")
         while(True):
             try:
                 await self.bot.send_message(self.bot.get_channel(str(226771859964690432)), str(i) + " testing the length of my dong, you know what I am saying?")
                 i+=1
-                await asyncio.sleep(5.0)
+                await asyncio.sleep(10.2)
             except:
                 print("replyLoop error")
                 self.bot.loop.create_task(self.replyLoop())
                 break
+        print("\n\n\n\n" + str(self.bot.user) + " has broken out of the reply loop!\n\n\n\n")
 
     async def progressLoop(self):
         while(True):
@@ -32,7 +37,7 @@ class maid_cog:
                 if(self.king):
                     await self.bot.send_message(self.bot.get_channel(str(226804409659686913)), "t!rep " + self.king)
                     await self.bot.send_message(self.bot.get_channel(str(226804409659686913)), "t!daily " + self.king)
-                await asyncio.sleep(600)
+                await asyncio.sleep(1800)
             except:
                 print("progressLoop error:")
                 self.bot.loop.create_task(self.progressLoop())
